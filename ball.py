72d4d16a14cb35b20649d3e8e0b8856cff7539c3
minkeuncho <minkeun123@naver.com>
2023-10-18 18:42:19 +0900

    Drill 10 제출

---
+++ ball.py
@@ -0,0 +1,21 @@
+from pico2d import load_image
+import game_world
+
+class Ball:
+    image = None
+    radian = 0
+    def __init__(self, x = 400, y = 300, velocity = 1):
+        if Ball.image == None:
+            Ball.image = load_image('ball21x21.png')
+        self.x, self.y, self.velocity = x, y, velocity
+
+    def draw(self):
+        # self.image.draw(self.x, self.y)
+        self.image.clip_composite_draw(0, 0, 21, 21, self.radian, '', self.x, self.y)
+
+    def update(self):
+        self.radian += 10
+        self.x += self.velocity
+
+        if self.x < 50 or self.x > 800 - 50:
+            game_world.remove_object(self)
---
+++ control_boy.py
@@ -0,0 +1,70 @@
+from pico2d import *
+
+import game_world
+from grass import Grass
+from grass import Grass02
+from boy import Boy
+
+
+# Game object class here
+
+
+def handle_events():
+    global running
+
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            running = False
+        else:
+            boy.handle_event(event)
+
+
+def reset_world():
+    global running
+    global grass
+    global grass02
+    global team
+    global boy
+
+    running = True
+
+    grass = Grass()
+    game_world.add_object(grass, 1)
+
+    grass02 = Grass02()
+    game_world.add_object(grass02, 0)
+
+    boy = Boy()
+    game_world.add_object(boy, 0)
+    
+
+
+
+def update_world():
+    game_world.update()
+    # for o in world:
+    #     o.update()
+    # pass
+
+
+def render_world():
+    clear_canvas()
+    game_world.render()
+    # for o in world:
+    #     o.draw()
+    update_canvas()
+
+
+open_canvas()
+reset_world()
+# game loop
+while running:
+    handle_events()
+    update_world()
+    render_world()
+    delay(0.01)
+# finalization code
+close_canvas()
