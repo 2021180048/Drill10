1e4b3bcb0195e17d3292d901aad64fa2ec22d1a5
minkeuncho <minkeun123@naver.com>
2023-10-21 21:38:26 +0900

    공회전 수식 수정

--- ball.py
+++ ball.py
@@ -3,6 +3,7 @@ import game_world
 
 class Ball:
     image = None
+    degree = 0
     radian = 0
     def __init__(self, x = 400, y = 300, velocity = 1):
         if Ball.image == None:
@@ -14,7 +15,8 @@ class Ball:
         self.image.clip_composite_draw(0, 0, 21, 21, self.radian, '', self.x, self.y)
 
     def update(self):
-        self.radian += 10
+        self.degree = (self.degree + 10) % 360
+        self.radian = self.degree * 180 / 3.141592
         self.x += self.velocity
 
         if self.x < 50 or self.x > 800 - 50:
