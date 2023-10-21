73d02d7db973e20c56f0c190f1c20a78774e9f77
minkeuncho <minkeun123@naver.com>
2023-10-21 22:30:29 +0900

    식 다시 수정

--- ball.py
+++ ball.py
@@ -12,13 +12,13 @@ class Ball:
 
     def draw(self):
         # self.image.draw(self.x, self.y)
-        self.image.clip_composite_draw(0, 0, 21, 21, self.radian, '', self.x, self.y)
+        if self.velocity > 0:
+            self.image.clip_composite_draw(0, 0, 21, 21, -self.radian, '', self.x, self.y)
+        if self.velocity < 0:
+            self.image.clip_composite_draw(0, 0, 21, 21, self.radian, '', self.x, self.y)
 
     def update(self):
-        if self.velocity > 0:
-            self.degree = (self.degree - 20) % 360
-        if self.velocity < 0:
-            self.degree = (self.degree + 20) % 360
+        self.degree = (self.degree + 20) % 360
         
         self.radian = self.degree * 3.141592 / 180
         self.x += self.velocity
